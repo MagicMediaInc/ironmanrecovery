67607008a0d97c25dc94aee9c21f69ee3c556ace
AlexanderZon <alexisanderson@ovi.com>
2014-10-21 10:12:30 -0430

    Archive Debbug

--- wp-content/themes/neighborhood/archive.php
+++ wp-content/themes/neighborhood/archive.php
@@ -8,7 +8,7 @@
 	$left_sidebar = $options['archive_sidebar_left'];
 	$right_sidebar = $options['archive_sidebar_right'];
 	$blog_type = $options['archive_display_type'];
-	
+	var_dump($options);
 	$page_wrap_class = '';
 	if ($sidebar_config == "left-sidebar") {
 	$page_wrap_class = 'has-left-sidebar has-one-sidebar row';
