39fe5a5e356979e8e4d1a816f1900fe618455468
AlexanderZon <alexisanderson@ovi.com>
2014-10-27 14:23:13 -0430

    More Translations

--- wp-content/themes/neighborhood/woocommerce/cart/totals.php
+++ wp-content/themes/neighborhood/woocommerce/cart/totals.php
@@ -19,7 +19,7 @@ $available_methods = $woocommerce->shipping->get_available_shipping_methods();
 
 	<?php if ( ! $woocommerce->shipping->enabled || $available_methods || ! $woocommerce->customer->get_shipping_country() || ! $woocommerce->customer->has_calculated_shipping() ) : ?>
 
-		<h3><?php _e( 'Resumen da compra', 'swiftframework' ); ?></h3>
+		<h3><?php _e( 'Resumo da compra', 'swiftframework' ); ?></h3>
 
 		<table cellspacing="0">
 			<tbody>
