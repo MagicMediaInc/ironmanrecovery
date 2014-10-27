7a2b9166445f180f11a55eb7ad381438fd40a3ee
AlexanderZon <alexisanderson@ovi.com>
2014-10-27 09:54:23 -0430

    order review debug

--- wp-content/themes/neighborhood/woocommerce/checkout/review-order.php
+++ wp-content/themes/neighborhood/woocommerce/checkout/review-order.php
@@ -107,13 +107,14 @@ $available_methods = $woocommerce->shipping->get_available_shipping_methods();
 				<tr class="shipping">
 					<th><?php _e( 'Frete:', 'swiftframework' ); ?></th>
 					<td><?php 
-						$chosen_mthd = null;
+						/*$chosen_mthd = null;
 						foreach ( $available_methods as $method ):
 							if($method->id == $woocommerce->session->chosen_shipping_method)
 								$chosen_mthd = $method->label;
 						endforeach;
-						echo $chosen_mthd;
-					//echo $woocommerce->session->chosen_shipping_method; //woocommerce_get_template( 'cart/shipping-methods.php', array( 'available_methods' => $available_methods ) ); ?></td>
+						echo $chosen_mthd;*/
+					//echo $woocommerce->session->chosen_shipping_method; 
+						woocommerce_get_template( 'cart/shipping-methods.php', array( 'available_methods' => $available_methods ) ); ?></td>
 				</tr>
 
 				<?php do_action('woocommerce_review_order_after_shipping'); ?>
