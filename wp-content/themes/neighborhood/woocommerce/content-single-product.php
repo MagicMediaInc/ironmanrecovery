2b66edd5955dcc364e32ddd6663657e57c187950
Robert Da Corte <robertdacorte@gmail.com>
2014-12-05 09:44:11 -0430

    add features tab

--- wp-content/themes/neighborhood/woocommerce/content-single-product.php
+++ wp-content/themes/neighborhood/woocommerce/content-single-product.php
@@ -129,7 +129,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 		<?php
 
 		$tabs = apply_filters( 'woocommerce_product_tabs', array() );
-
+		var_dump($tabs);
 		if ( ! empty( $tabs ) ) : ?>
 
 			<div class="woocommerce-tabs">
