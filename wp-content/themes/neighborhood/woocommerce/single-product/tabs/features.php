75342397287975eada9b20c3e6b594697db2966d
Robert Da Corte <robertdacorte@gmail.com>
2014-12-05 11:49:36 -0430

    add features tab

--- wp-content/themes/neighborhood/woocommerce/single-product/tabs/features.php
+++ wp-content/themes/neighborhood/woocommerce/single-product/tabs/features.php
@@ -9,11 +9,11 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-global $woocommerce, $post;
+global $woocommerce, $post, $products;
 
-$heading = esc_html( apply_filters('woocommerce_product_description_heading', __( 'Caracteristicas do Produto', 'woocommerce' ) ) );
+// $heading = esc_html( apply_filters('woocommerce_product_feature_heading', __( 'Caracteristicas do Produto', 'woocommerce' ) ) );
 ?>
 
-<h2><?php echo $heading; ?></h2>
+<h2><?php echo 'Caracteristicas do Produto'; ?></h2>
 
 <?php the_content(); ?>
