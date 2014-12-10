cbabae816fb68d239bd3e0f886e448511c466bb0
AlexanderZon <alexisanderson@ovi.com>
2014-12-10 14:11:28 -0430

    clickmap template fix

--- wp-content/plugins/mymail2018/views/newsletter/template.php
+++ wp-content/plugins/mymail2018/views/newsletter/template.php
@@ -370,7 +370,7 @@ wp_editor('', 'mymail-editor', array(
 	</div>
 	<p class="device-info"><?php _e('Your email may look different on mobile devices', 'mymail'); ?></p>
 </div></div>
-<textarea id="content" class="hidden" autocomplete="off" name="content" ><?php echo $post->post_content ?></textarea>
+<!--<textarea id="content" class="hidden" autocomplete="off" name="content" ><?php echo $post->post_content ?></textarea>-->
 <textarea id="modules" class="hidden" autocomplete="off"><?php echo $modules ?></textarea>
 <textarea id="head" name="mymail_data[head]" class="hidden" autocomplete="off"><?php echo isset($this->post_data['head']) ? $this->post_data['head'] : $this->templateobj->get_head(); ?></textarea>
 <?php echo "Fin"; ?>
