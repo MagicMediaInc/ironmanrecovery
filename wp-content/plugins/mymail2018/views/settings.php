f9435e7e88a75ef39cf02b0bb2aff7bd6bf8f139
AlexanderZon <alexisanderson@ovi.com>
2014-12-10 11:47:22 -0430

    news settings fix

--- wp-content/plugins/mymail2018/views/settings.php
+++ wp-content/plugins/mymail2018/views/settings.php
@@ -30,7 +30,7 @@
 	$active = count(mymail_get_active_campaigns());
 		
 	$templatefiles = mymail('templates')->get_files(mymail_option('default_template'));
-	$newletters = get_posts( array( 'post_type' => 'newletter') );
+	$newletters = get_posts( array( 'post_type' => 'newsletter') );
 	$timeformat = get_option('date_format').' '.get_option('time_format');
 	$timeoffset = get_option('gmt_offset')*3600;
 
